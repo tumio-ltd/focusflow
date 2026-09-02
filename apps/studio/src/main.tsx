8177d8f4be604dbdfcb56e9537871e1b5fad2cd3
chenxiang <1103877588@qq.com>
2026-09-02 21:36:45 +0800

    chore(studio): disable jitter radar auto-logging while preserving utility module

--- apps/studio/src/main.tsx
+++ apps/studio/src/main.tsx
@@ -4,10 +4,8 @@ import { ThemeProvider } from 'next-themes';
 import './i18n';
 import App from './App';
 import './index.css';
-import { initJitterRadar } from './utils/jitterRadar';
-
-// 启动全局抖动侦测雷达
-initJitterRadar();
+// import { initJitterRadar } from './utils/jitterRadar';
+// initJitterRadar();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
